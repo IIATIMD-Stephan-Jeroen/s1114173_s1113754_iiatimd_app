f68d883624d908bd22420c1d379d49db27038b8f
Stephan <s1114173@student.hsleiden.nl>
2021-06-03 14:59:58 +0200

    Logging in works now and brings you to the main page

--- Inventory/app/src/main/java/com/example/inventory/LoginActivity.java
+++ Inventory/app/src/main/java/com/example/inventory/LoginActivity.java
@@ -9,11 +9,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.api.model.Login;
+import com.example.api.model.User;
+import com.example.api.service.UserClient;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl("https://iiatimd-stephan-jeroen.herokuapp.com/api/")
+            .addConverterFactory(GsonConverterFactory.create());
+
+    Retrofit retrofit = builder.build();
+
+    UserClient userClient = retrofit.create(UserClient.class);
 
     private Button loginButton;
     private EditText mailInput;
@@ -31,7 +45,31 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                login();
+            }
+        });
+    }
+
+    private void login(){
+
+        Login login = new Login(mailInput.getText().toString(), passwordInput.getText().toString());
+        Call<User> call = userClient.login(login);
+        
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Email or Password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "error", Toast.LENGTH_SHORT).show();
             }
         });
     }
