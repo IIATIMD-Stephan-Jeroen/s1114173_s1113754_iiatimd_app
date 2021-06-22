4923df01db9a51c052b1f17e3a89bdb56ad41744
Stephan <s1114173@student.hsleiden.nl>
2021-06-22 11:34:37 +0200

    recyclerview voor bags toegevoegd

--- Inventory/app/src/main/java/com/example/inventory/activity/MainActivity.java
+++ Inventory/app/src/main/java/com/example/inventory/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.inventory.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,14 +13,34 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.inventory.R;
+import com.example.inventory.adapter.BagAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView bagRecyclerView;
+    private RecyclerView.Adapter bagRecyclerViewAdapter;
+    private RecyclerView.LayoutManager bagLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bagRecyclerView = findViewById(R.id.bagRecyclerView);
+        bagLayoutManager = new LinearLayoutManager(this);
+        bagRecyclerView.setLayoutManager(bagLayoutManager);
+        bagRecyclerView.hasFixedSize();
+
+        String[] names = new String[200];
+        String[] descriptions = new String[200];
+
+        for(int i = 0; i < 200; i++){
+            names[i] = "NAME " + i;
+            descriptions[i] = "DESCRIPTION " + i;
+        }
+
+        bagRecyclerViewAdapter = new BagAdapter(names, descriptions);
+        bagRecyclerView.setAdapter(bagRecyclerViewAdapter);
     }
 
 
--- Inventory/app/src/main/java/com/example/inventory/adapter/BagAdapter.java
+++ Inventory/app/src/main/java/com/example/inventory/adapter/BagAdapter.java
@@ -1,13 +1,25 @@
 package com.example.inventory.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.inventory.R;
 
 public class BagAdapter extends RecyclerView.Adapter<BagAdapter.BagViewHolder> {
+
+    private String[] names;
+    private String[] descriptions;
+
+    public BagAdapter(String[] names, String[] descriptions){
+        this.names = names;
+        this.descriptions = descriptions;
+    }
 
     public static class BagViewHolder extends RecyclerView.ViewHolder{
 
@@ -20,4 +32,25 @@ public class BagAdapter extends RecyclerView.Adapter<BagAdapter.BagViewHolder> {
             bagDesc = v.findViewById(R.id.bagDescription);
         }
     }
+
+    @NonNull
+    @Override
+    public BagViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.bag_card, parent, false);
+        BagViewHolder bagViewHolder = new BagViewHolder(v);
+        return bagViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BagAdapter.BagViewHolder holder, int position) {
+        holder.bagName.setText(names[position]);
+        holder.bagDesc.setText(descriptions[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.length;
+    }
+
+
 }
