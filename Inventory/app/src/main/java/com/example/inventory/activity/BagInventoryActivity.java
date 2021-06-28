d2a9a0be0201489b16bf2724bcccd2cad6334242
Stephan <s1114173@student.hsleiden.nl>
2021-06-28 14:41:46 +0200

    added inputs in add bag screen

--- Inventory/app/src/main/java/com/example/inventory/activity/BagInventoryActivity.java
+++ Inventory/app/src/main/java/com/example/inventory/activity/BagInventoryActivity.java
@@ -3,14 +3,20 @@ package com.example.inventory.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.inventory.R;
 
 public class BagInventoryActivity extends AppCompatActivity {
 
+    private EditText insertBagName;
+    private EditText insertBagDesc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bag_inventory);
+
+
     }
 }
