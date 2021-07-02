7c5975281dd202cc16bee4931eef9b864da078f6
StephanV2 <stephanhouwaart@hotmail.com>
2021-07-02 20:51:31 +0200

    Individual bags are now clickable

--- Inventory/app/src/main/java/com/example/inventory/activity/BagInventoryActivity.java
+++ Inventory/app/src/main/java/com/example/inventory/activity/BagInventoryActivity.java
@@ -2,19 +2,27 @@ package com.example.inventory.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.inventory.R;
 
 public class BagInventoryActivity extends AppCompatActivity {
 
+    TextView inventoryHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bag_inventory);
 
+        inventoryHolder = findViewById(R.id.inventoryHolder);
 
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("bag_name");
+
+        inventoryHolder.setText(name);
     }
 }
